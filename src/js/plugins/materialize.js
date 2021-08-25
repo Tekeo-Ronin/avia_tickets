7fb6bcb528fadd358cf4d6a3649dadf86c6d8e4b
Volodymyr Korzhevskyi <korzhevskyiv@gmail.com>
2021-08-25 13:02:16 +0300

    finished creating UI

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -6,6 +6,31 @@ import 'materialize-css/dist/js/materialize.min.js';
 const select = document.querySelectorAll('select');
 M.FormSelect.init(select);
 
+//init autocomplite
+const autocomplete = document.querySelectorAll('.autocomplete');
+M.Autocomplete.init(autocomplete, {
+  data: {
+    "Apple": null,
+    "Microsoft": null,
+    "Google": 'https://placehold.it/250x250'
+  },
+});
+
+//init datepicker
+
+const datepicker = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepicker, {
+  showClearBtn: true,
+});
+
 export function getSelectInstance(elem) {
   return M.FormSelect.getInstance(elem);
+}
+
+export function getAutocompleteInstance(elem) {
+  return M.Autocomplete.init(elem);
+}
+
+export function getDatepickerInstance(elem){
+  return M.Datepicker.init(elem);
 }
